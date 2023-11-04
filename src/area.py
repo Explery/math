5c7874fb52bfe1fac303e0f67f4591862613d33d
owen <56456776+JetsadaWijit@users.noreply.github.com>
2023-11-04 03:55:20 +0000

    add area

--- src/area.py
+++ src/area.py
@@ -9,3 +9,15 @@ def getRectangle(x, y):
 # x = base, y = height
 def getTriangle(x, y):
     return (x*y)/2
+
+# x = large diagonal, y = small diagonal
+def getRhombus(x, y):
+    return (x*y)/2
+
+# x = large side, y = small side, z = height
+def getTrapezoid(x, y ,z):
+    return ((x+y)/2)*z
+
+# x = perimeter, y = apothem
+def getRegularPolygon(x, y):
+    return (x/2)*y
